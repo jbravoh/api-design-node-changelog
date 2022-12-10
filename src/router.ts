87178b84cc7a991cfbb4e99c819c4e856ba11848
jbravoh <jbravoharriott@gmail.com>
2022-12-10 21:05:19 +0000

    import application router, add routes & add nodemon

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,40 @@ import { Router } from "express";
 
 const router = Router()
 
-// <=== PRODUCT ===>
+/**
+ * Product
+ */
 
-router.get('/product', () => {})
+router.get('/product', (req, res) => {
+    res.json({message: 'hello'})
+})
 router.get('/product/:id', () => {})
 router.post('/product', () => {})
 router.put('/product/:id', () => {})
 router.delete('/product/:id', () => {})
 
+/**
+ * Update
+ */
 
+ router.get('/update', () => {})
+ router.get('/update/:id', () => {})
+ router.post('/update', () => {})
+ router.put('/update/:id', () => {})
+ router.delete('/update/:id', () => {})
+ 
+
+/**
+ * Update Point
+ */
+
+ router.get('/updatepoint', (req, res) => {
+    res.json({message: 'hello'})
+ })
+ router.get('/updatepoint/:id', () => {})
+ router.post('/updatepoint', () => {})
+ router.put('/updatepoint/:id', () => {})
+ router.delete('/updatepoint/:id', () => {})
+ 
+
+ export default router
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import router from './router'
 
 const app = express();
 
-// app is the entire api
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
   res.json({ message: "hello" });
 });
+
+app.use('/api', router)
 
 
 export default app;
