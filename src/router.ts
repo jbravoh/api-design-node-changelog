3f4167ad8e9ec6139b811cea8448cc940e72636b
jbravoh <jbravoharriott@gmail.com>
2022-12-10 20:14:49 +0000

    reate index, server & router page & add prisma

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router()
+
+// <=== PRODUCT ===>
+
+router.get('/product', () => {})
+router.get('/product/:id', () => {})
+router.post('/product', () => {})
+router.put('/product/:id', () => {})
+router.delete('/product/:id', () => {})
+
+
