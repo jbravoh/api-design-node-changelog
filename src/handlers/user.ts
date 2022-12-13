decc8c01c692499c407e6c6b9c0954cf23695094
jbravoh <jbravoharriott@gmail.com>
2022-12-13 19:44:30 +0000

    create PrismaClient helper, create signin function

---
+++ src/handlers/user.ts
@@ -0,0 +1,36 @@
+import prisma from '../db'
+import { comparePasswords, createJWT, hashPassword } from '../modules/auth'
+
+export const createNewUser = async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            username: req.body.username, 
+            password: await hashPassword(req.body.password)
+        }
+    })
+
+    const token = createJWT(user)
+    res.json({ token }) 
+}
+
+export const signin = async (req, res) => {
+    // find the user based on the username inputted
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username
+        }
+    })
+
+    // compare the imputted password with the hash password stored
+    const isValid = await comparePasswords(req.body.password, user.password)
+
+    if (!isValid) {
+        res.status(401)
+        res.json({message: 'Invalid username or password'})
+        return
+    }
+
+    // create token if valid
+    const token = createJWT(user)
+    res.json({ token }) 
+}
